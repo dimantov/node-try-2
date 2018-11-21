ac28fe32d1339997c3ec883d4c48d094287cb4e5
Dmitri Antonov <dmitriantonov@Dmitris-MacBook-Pro.local>
2018-11-21 20:51:31 +0200

    Added update script

---
+++ playground/mongo-update.js
@@ -0,0 +1,35 @@
+const {MongoClient, ObjectID} = require('mongodb');
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  // db.collection('Todos').findOneAndUpdate({name:'Dmitri'},{$set: {name:'Roman'}},(err, res) => {
+  //   if(err){
+  //     return console.log(err)
+  //   }
+  //   console.log(res);
+  // })
+
+  db.collection('Todos').updateOne({name:'Roman'}, {$set:{name:'Dmitri'}}).then((res)=> {
+    console.log(res);
+  },(err) => {
+    console.log(err);
+  })
+
+
+  client.close();
+});
